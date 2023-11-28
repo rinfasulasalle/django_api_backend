275c444d8f122a95891331a8742dca94c36409e4
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-28 00:41:33 +0000

    modifico settings

--- drf/settings.py
+++ drf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^h*jijca$^er!d*36ic#5lj!khc83u8^f0vupyuq!bz=)tx55o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
 ]
 
 MIDDLEWARE = [
