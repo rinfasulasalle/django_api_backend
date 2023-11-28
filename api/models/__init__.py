e72d5ee0c9d281a6c8cd5092840dd16478994747
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-28 14:52:02 +0000

    Migración Contrato

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -21,9 +21,8 @@ from .dropdowns import (
 from .usuario import Usuario
 from .trabajador import Trabajador
 from .sueldo import Sueldo
-'''
 from .contrato import Contrato
-from .cuenta_bancaria import CuentaBancaria
+'''from .cuenta_bancaria import CuentaBancaria
 from .direccion import Direccion
 from .estudio import Estudio
 '''
---
+++ api/models/contrato.py
@@ -0,0 +1,28 @@
+# models/contrato.py
+from django.db import models
+from .dropdowns import (
+    DropdownTipoContrato,
+    DropdownContratoOpcion,
+    DropdownEmpleoTipo,
+    DropdownEmpleoSituacion,
+    DropdownAreas,
+    DropdownProyecto,
+    DropdownRolProyecto,
+)
+from .trabajador import Trabajador  # Asegúrate de importar el modelo correcto
+
+class Contrato(models.Model):
+    id = models.AutoField(primary_key=True)
+    trabajador = models.OneToOneField(Trabajador, on_delete=models.CASCADE, unique=True)
+    id_contrato_tipo = models.ForeignKey(DropdownTipoContrato, on_delete=models.CASCADE)
+    id_contrato_opcion = models.ForeignKey(DropdownContratoOpcion, on_delete=models.CASCADE)
+    id_empleo_tipo = models.ForeignKey(DropdownEmpleoTipo, on_delete=models.CASCADE)
+    id_empleo_situacion = models.ForeignKey(DropdownEmpleoSituacion, on_delete=models.CASCADE)
+    id_empleo_area = models.ForeignKey(DropdownAreas, on_delete=models.CASCADE)
+    id_empleo_proyecto = models.ForeignKey(DropdownProyecto, on_delete=models.CASCADE)
+    id_empleo_proyecto_rol = models.ForeignKey(DropdownRolProyecto, on_delete=models.CASCADE)
+    empleo_departamento = models.CharField(max_length=100, null=True, default=None)
+    empleo_cargo = models.CharField(max_length=100, default=None)
+
+    def __str__(self):
+        return f"Contrato: {self.id} - {self.trabajador.usuario_relacionado.id}"
