9219f76cf6482014999cf39f11b452d7028313a9
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-12-22 09:21:19 +0000

    Arreglo el detonador apra trabajadores

--- api/models/trabajador.py
+++ api/models/trabajador.py
@@ -1,17 +1,14 @@
-# models/trabajador.py
 from django.db import models
 from .usuario import Usuario
+from datetime import date
 
 class Trabajador(models.Model):
     usuario_relacionado = models.OneToOneField(Usuario, primary_key=True, on_delete=models.CASCADE)
-    #usuario_id = models.CharField(max_length=20, primary_key=True)
     trabajador_tipo_documento = models.CharField(max_length=100)
-    #trabajador_path_documento = models.CharField(max_length=255, default='PATH/noNe')
     trabajador_path_documento = models.FileField(upload_to='docs/identidades/', blank=True, null=True)
     trabajador_nacionalidad = models.CharField(max_length=100, default='No Especificado')
     trabajador_fecha_nacimiento = models.DateField()
     trabajador_ubigeo = models.CharField(max_length=255, default='No Especificado')
-    #trabajador_telefono = models.CharField(max_length=100)
     trabajador_sexo = models.CharField(
         max_length=15,
         choices=[
@@ -33,14 +30,32 @@ class Trabajador(models.Model):
         ],
         default='No Especificado'
     )
-    #trabajador_path_doc_estado_civil = models.CharField(max_length=255, default='PATH/noNe')
     trabajador_path_doc_estado_civil = models.FileField(upload_to='docs/estados_civiles/', blank=True, null=True)
     trabajador_fecha_ingreso_sistema = models.DateField()
     trabajador_fecha_ingreso = models.DateField()
-    trabajador_edad = models.IntegerField()
-    trabajador_record = models.DecimalField(max_digits=20, decimal_places=2)
-    trabajador_exp_previa = models.DecimalField(max_digits=20, decimal_places=2)
-    trabajador_total_anios_exp = models.DecimalField(max_digits=20, decimal_places=2)
-    
+    trabajador_edad = models.IntegerField(default=0)  # por defecto 0, se calculará con save()
+    trabajador_record = models.DecimalField(max_digits=5, decimal_places=2, default=0)  # por defecto 0, se calculará con save()
+    trabajador_exp_previa = models.DecimalField(max_digits=5, decimal_places=2)  # se ingresa manualmente, es un decimal
+    trabajador_total_anios_exp = models.DecimalField(max_digits=5, decimal_places=2, default=0)  # por defecto 0, se calculará con save()
+
+    def save(self, *args, **kwargs):
+        # 1. La fecha de ingreso al sistema es la fecha actual
+        self.trabajador_fecha_ingreso_sistema = date.today()
+
+        # 2. Calcular la edad en base a la fecha de nacimiento y la fecha actual
+        hoy = date.today()
+        edad = hoy.year - self.trabajador_fecha_nacimiento.year - ((hoy.month, hoy.day) < (self.trabajador_fecha_nacimiento.month, self.trabajador_fecha_nacimiento.day))
+        self.trabajador_edad = edad
+
+        # 3. Calcular trabajador_record
+        tiempo_trabajado = hoy - self.trabajador_fecha_ingreso
+        record = tiempo_trabajado.days / 365.25
+        self.trabajador_record = round(record, 2)
+
+        # 4. Calcular trabajador_total_anios_exp
+        self.trabajador_total_anios_exp = self.trabajador_record + float(self.trabajador_exp_previa)
+
+        super(Trabajador, self).save(*args, **kwargs)
+
     def __str__(self):
         return f"Trabajador: {self.usuario_relacionado}, {self.usuario_relacionado.usuario_nombres} {self.usuario_relacionado.usuario_apellido_paterno} {self.usuario_relacionado.usuario_apellido_materno}"
--- drf/settings.py
+++ drf/settings.py
@@ -153,7 +153,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+# Define el formato de fecha que prefieres
+DATE_INPUT_FORMATS = ['%d-%m-%Y']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
