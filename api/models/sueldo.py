ee2e71cb594b67070e4c15f6f39da525c1ef6277
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-12-22 06:08:46 +0000

    api/migrations/0015_remove_sueldo_sueldo_asigfam_porcentaje_and_more.py     - Remove field sueldo_asigfam_porcentaje from sueldo     - Remove field sueldo_asignacion_familiar from sueldo     - Remove field sueldo_bono_porcentaje from sueldo     - Add field sueldo_monto_asigfam to sueldo     - Add field sueldo_opc_asigfam to sueldo     - Add field sueldo_opc_bono to sueldo     - Add field sueldo_porcentaje_bono to sueldo     - Alter field sueldo_anual on sueldo     - Alter field sueldo_mensual on sueldo     - Alter field sueldo_monto_bono on sueldo

--- api/models/sueldo.py
+++ api/models/sueldo.py
@@ -1,19 +1,36 @@
 # models/sueldo.py
 from django.db import models
-from .trabajador import Trabajador  # Asegúrate de importar el modelo correcto
+from .trabajador import Trabajador
 
 class Sueldo(models.Model):
     trabajador = models.OneToOneField(Trabajador, on_delete=models.CASCADE, unique=True)
     id = models.AutoField(primary_key=True)
     sueldo_valor_basico = models.DecimalField(max_digits=20, decimal_places=2)
-    sueldo_asigfam_porcentaje = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
-    sueldo_asignacion_familiar = models.DecimalField(max_digits=20, decimal_places=2, null=True, blank=True)
-    sueldo_bono_porcentaje = models.DecimalField(max_digits=20, decimal_places=2, default=0)
-    sueldo_monto_bono = models.DecimalField(max_digits=20, decimal_places=2)
-    sueldo_mensual = models.DecimalField(max_digits=20, decimal_places=2)
-    sueldo_anual = models.DecimalField(max_digits=20, decimal_places=2)
+    sueldo_opc_asigfam = models.BooleanField(default=False)
+    sueldo_opc_bono = models.BooleanField(default=False)
+    sueldo_porcentaje_bono = models.DecimalField(max_digits=3, decimal_places=2, default=0.0)
+    sueldo_monto_asigfam = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
+    sueldo_monto_bono = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
+    sueldo_mensual = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
+    sueldo_anual = models.DecimalField(max_digits=20, decimal_places=2, default=0.0)
 
+    def save(self, *args, **kwargs):
+        # Calcula sueldo_monto_asigfam y sueldo_monto_bono
+        if not self.sueldo_opc_asigfam:
+            self.sueldo_monto_asigfam = 0.0
+        else:
+            self.sueldo_monto_asigfam = self.sueldo_valor_basico * 0.2
+
+        if not self.sueldo_opc_bono:
+            self.sueldo_monto_bono = 0.0
+        else:
+            self.sueldo_monto_bono = self.sueldo_valor_basico * self.sueldo_porcentaje_bono
+
+        # Calcula sueldo_mensual y sueldo_anual
+        self.sueldo_mensual = self.sueldo_valor_basico + self.sueldo_monto_asigfam + self.sueldo_monto_bono
+        self.sueldo_anual = self.sueldo_mensual * 14
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Sueldo: {self.id} - {self.trabajador.usuario_relacionado.id}"
-
